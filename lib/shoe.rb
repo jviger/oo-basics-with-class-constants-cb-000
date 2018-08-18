fd63169989a8b68f1c4b9628c7cb27064ed1779d
Jason Viger <jason.viger@gmail.com>
2018-08-18 07:57:18 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,7 +6,9 @@ class Shoe
   
   def initialize(brand)
     @brand = brand
+    if(!BRANDS.include?[brand])
     BRANDS << brand
+  end
   end
 
   def cobble
@@ -14,9 +16,6 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
   
-  def brand=(brand)
-    @brand = brand
-   
-  end
+  
 
 end
